f6cf7aeb989c160c772267bf82ae408d9d43974b
Melissa Bagwell <mhbagwell@mail.fhsu.edu>
2023-05-09 16:42:22 -0400

    updated allowedOrigins

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,6 +1,6 @@
 
 const allowedOrigins = [
-    'https://final-projectmelissabagwell.mhbagwell.repl.co',
+    'https://final-project.mhbagwell.repl.co',
     'https://dazzling-snickerdoodle-777101.netlify.app',
     'http://127.0.0.1:5500',
     'http://localhost:3500',
