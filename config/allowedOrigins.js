d407dc0874368cca8e5db83aa029a772e0934308
Melissa Bagwell <mhbagwell@mail.fhsu.edu>
2023-05-09 17:11:35 -0400

    updated link

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,6 +1,6 @@
 
 const allowedOrigins = [
-    'https://horse-friendly-bench.glitch.me',
+    'https://marmalade-shining-healer.glitch.me/',
     'https://final-project.mhbagwell.repl.co',
     'https://dazzling-snickerdoodle-777101.netlify.app',
     'http://127.0.0.1:5500',
