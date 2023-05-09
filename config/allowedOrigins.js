61b3921f6250166cec483509e25c80d6fff94472
Melissa Bagwell <mhbagwell@mail.fhsu.edu>
2023-05-09 17:20:43 -0400

    updated allowedOrigins

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,7 +1,6 @@
 
 const allowedOrigins = [
     'https://marmalade-shining-healer.glitch.me/',
-    'https://final-project.mhbagwell.repl.co',
     'https://dazzling-snickerdoodle-777101.netlify.app',
     'http://127.0.0.1:5500',
     'http://localhost:3500',
